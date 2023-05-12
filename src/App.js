a7b577e3d93e1891e6c55645694764ae4fcf31a3
Tob Stoic <rafaeltobass@gmail.com>
2023-05-12 02:06:07 -0500

    estructuras de los componentes en App.js

--- src/App.js
+++ src/App.js
@@ -4,22 +4,26 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edita el archivo <code>src/App.js</code> y guarda para recargar.
-        </p>
-        <a
-          className="App-link"
-          href="https://platzi.com/reactjs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <TodoItem/>
+
+
     </div>
   );
+}
+
+function TodoItem() {
+  return (
+
+    <li>
+
+      <span> v </span>
+      <p>Loorar con la llorona </p>
+      <span> X </span>
+
+    </li>
+
+  )
 }
 
 export default App;
